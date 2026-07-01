d80ce60ac8598517969a7f4f9c502ca411bea812
Simon Cropp <simon.cropp@gmail.com>
2026-07-01 11:18:50 +1000

    cleanup

---
+++ src/Tests/ConvertExcelSnapshots.cs
@@ -0,0 +1,35 @@
+﻿#if NET48 && DEBUG
+[TestFixture]
+[Apartment(ApartmentState.STA)]
+public class ConvertExcelSnapshots
+{
+    [Test]
+    [Explicit]
+    public void Run()
+    {
+        var directory = ProjectFiles.ProjectDirectory;
+        var imageFiles = Directory.EnumerateFiles(directory, "*.png").ToList();
+        foreach (var file in imageFiles)
+        {
+            File.Delete(file);
+        }
+
+        ExcelRender.Convert(Path.Combine(directory, "sample.xlsx"));
+        var excelFiles = Directory.EnumerateFiles(directory, "*.verified.xlsx").ToList();
+        foreach (var file in excelFiles)
+        {
+            if (Path.GetFileName(file).StartsWith('~'))
+            {
+                continue;
+            }
+
+            if (!Path.GetFileName(file).Contains("Net9"))
+            {
+                continue;
+            }
+
+            ExcelRender.Convert(file);
+        }
+    }
+}
+#endif
--- src/Tests/ExcelRender.cs
+++ src/Tests/ExcelRender.cs
@@ -6,40 +6,6 @@ using System.Drawing.Imaging;
 using System.Windows.Forms;
 using Application = Microsoft.Office.Interop.Excel.Application;
 
-[TestFixture]
-[Apartment(ApartmentState.STA)]
-public class ConvertExcelSnapshots
-{
-    [Test]
-    [Explicit]
-    public void Run()
-    {
-        var directory = ProjectFiles.ProjectDirectory;
-        var imageFiles = Directory.EnumerateFiles(directory, "*.png").ToList();
-        foreach (var file in imageFiles)
-        {
-            File.Delete(file);
-        }
-
-        ExcelRender.Convert(Path.Combine(directory, "sample.xlsx"));
-        var excelFiles = Directory.EnumerateFiles(directory, "*.verified.xlsx").ToList();
-        foreach (var file in excelFiles)
-        {
-            if (Path.GetFileName(file).StartsWith('~'))
-            {
-                continue;
-            }
-
-            if (!Path.GetFileName(file).Contains("Net9"))
-            {
-                continue;
-            }
-
-            ExcelRender.Convert(file);
-        }
-    }
-}
-
 public static class ExcelRender
 {
     public static void Convert(string excelPath)
